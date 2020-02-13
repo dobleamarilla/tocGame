421f9be5fc3073c53ed45300d48a2af502f0f7df
Eze <ezequielucho@gmail.com>
2020-02-13 20:07:38 +0100

    Update enviarDatos.js

--- web/js/sincro/enviarDatos.js
+++ web/js/sincro/enviarDatos.js
@@ -1,63 +1,56 @@
-async function enviarTickets(arrayTicket)
-{
-    var devolver = new Promise((dev, rej)=>{
-        for(let i = 0; i < arrayTicket.length; i++)
-        {
-                db.parametros.toArray().then(infoParams=>{
-                    let infoTicket = arrayTicket[i];
-                    let database = infoParams[0].database;
-                    let nombreTabla = `[V_Venut_${infoTicket.timestamp.getFullYear()}-${limpiarMes(infoTicket.timestamp)}]`;
-                    db.parametros.toArray().then(parametros=>{
-                        if(!modoDesarrollador)
-                        {
-                            console.log("TRAZA 3");
-                            socket.emit('guardar-ticket', {
-                                codigoTienda: parametros[0].codigoTienda,
-                                idDependienta: infoTicket.idTrabajador,
-                                idTicket: infoTicket.idTicket,
-                                tipoVenta: 'V',
-                                cesta: modificarCestaEnviarTicket(infoTicket),
-                                nombreTabla: nombreTabla,
-                                fecha: infoTicket.timestamp.toISOString(),
-                                database: database
-                            });
-                            dev(true);
-                        }
-                        else
-                        {
-                            notificacion('¡Modo desarrollador activado!', 'warning');
-                            dev(false);
-                        }
-                        
-                    }).catch(err=>{
-                        console.log(err);
-                        notificacion('Error en db.parametros de enviarTicket', 'error');
+async function enviarTickets(arrayTicket) {
+    var devolver = new Promise((dev, rej) => {
+        for (let i = 0; i < arrayTicket.length; i++) {
+            db.parametros.toArray().then(infoParams => {
+                let infoTicket = arrayTicket[i];
+                let database = infoParams[0].database;
+                let nombreTabla = `[V_Venut_${infoTicket.timestamp.getFullYear()}-${limpiarMes(infoTicket.timestamp)}]`;
+                db.parametros.toArray().then(parametros => {
+                    if (!modoDesarrollador) {
+                        console.log("TRAZA 3");
+                        let auxObjeto = {
+                            codigoTienda: parametros[0].codigoTienda,
+                            idDependienta: infoTicket.idTrabajador,
+                            idTicket: infoTicket.idTicket,
+                            tipoVenta: 'V',
+                            cesta: modificarCestaEnviarTicket(infoTicket),
+                            nombreTabla: nombreTabla,
+                            fecha: infoTicket.timestamp.toISOString(),
+                            database: database
+                        };
+                        console.log(auxObjeto);
+                        socket.emit('guardar-ticket', auxObjeto);
+                        dev(true);
+                    }
+                    else {
+                        notificacion('¡Modo desarrollador activado!', 'warning');
                         dev(false);
-                    });
-                }).catch(err=>{
+                    }
+
+                }).catch(err => {
                     console.log(err);
-                    notificacion('Error en get parametros en enviarTicket()', 'error');
+                    notificacion('Error en db.parametros de enviarTicket', 'error');
+                    dev(false);
                 });
+            }).catch(err => {
+                console.log(err);
+                notificacion('Error en get parametros en enviarTicket()', 'error');
+            });
             infoTicket = [];
         }
         dev(true);
     });
     return devolver;
 }
-function limpiarMes(date)
-{
+function limpiarMes(date) {
     var month = date.getMonth() + 1;
     return month < 10 ? '0' + month : '' + month;
-}  
+}
 
-function modificarCestaEnviarTicket(infoTicket) /* Esta función limpia las promociones y deja en la cesta los precios reales de los productos */
-{
-    for(let i = 0; i < infoTicket.cesta.length; i++)
-    {
-        if(infoTicket.cesta[i].promocion === 1)
-        {
-            if(infoTicket.cesta[i].infoArticulosPromo.tipoPromo === 'individual')
-            {
+function modificarCestaEnviarTicket(infoTicket) /* Esta función limpia las promociones y deja en la cesta los precios reales de los productos */ {
+    for (let i = 0; i < infoTicket.cesta.length; i++) {
+        if (infoTicket.cesta[i].promocion === 1) {
+            if (infoTicket.cesta[i].infoArticulosPromo.tipoPromo === 'individual') {
                 infoTicket.cesta.push({
                     idArticulo: infoTicket.cesta[i].infoArticulosPromo.idPrincipal,
                     nombreArticulo: 'nuncaSeVe',
@@ -69,10 +62,8 @@ function modificarCestaEnviarTicket(infoTicket) /* Esta función limpia las prom
                     idLinea: -1
                 });
             }
-            else
-            {
-                if(infoTicket.cesta[i].infoArticulosPromo.tipoPromo === 'combo')
-                {
+            else {
+                if (infoTicket.cesta[i].infoArticulosPromo.tipoPromo === 'combo') {
                     infoTicket.cesta.push({
                         idArticulo: infoTicket.cesta[i].infoArticulosPromo.idPrincipal,
                         nombreArticulo: 'nuncaSeVe',
@@ -94,13 +85,12 @@ function modificarCestaEnviarTicket(infoTicket) /* Esta función limpia las prom
                         idLinea: -1
                     });
                 }
-                else
-                {
+                else {
                     console.log("Error, buscar traza en modificarCestaEnviarTicket()");
                     notificacion('Error al enviar el ticket al servidor', 'error');
                 }
             }
-            infoTicket.cesta.splice(i,1);
+            infoTicket.cesta.splice(i, 1);
         }
     }
     return infoTicket.cesta;
