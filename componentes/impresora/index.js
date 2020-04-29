489f5c5f2ab14476a2fff2aeb949cc669718bbe5
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-04-29 19:58:58 +0200

    Update index.js

--- componentes/impresora/index.js
+++ componentes/impresora/index.js
@@ -9,7 +9,7 @@ const TIPO_ENTRADA_DINERO = 2;
 var imprimirTicketVenta = function (event, numFactura, arrayCompra, total, visa, tiposIva, cabecera, pie, nombreDependienta) {
     try {
         //var device = new escpos.USB('0x4B8', '0x202'); //USB
-        var device = new escpos.Serial('/dev/ttyS1', {
+        var device = new escpos.Serial('/dev/ttyS0', {
             baudRate: 14400,
             stopBit: 2
         }) //SERIE
