3e87b37aa9e409f1f1b5af4065c85d989f9c73c5
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-04-29 19:45:19 +0200

    Update index.js

--- componentes/impresora/index.js
+++ componentes/impresora/index.js
@@ -9,7 +9,10 @@ const TIPO_ENTRADA_DINERO = 2;
 var imprimirTicketVenta = function (event, numFactura, arrayCompra, total, visa, tiposIva, cabecera, pie, nombreDependienta) {
     try {
         //var device = new escpos.USB('0x4B8', '0x202'); //USB
-        var device = new escpos.Serial('/dev/ttyS1') //SERIE
+        var device = new escpos.Serial('/dev/ttyS1', {
+            baudRate: 14400,
+            stopBit: 2
+        }) //SERIE
         //var options = { encoding: "latin1" };
         var printer = new escpos.Printer(device);
 
